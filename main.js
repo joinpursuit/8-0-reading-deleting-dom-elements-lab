da491681c66893e12fb7dde7125521ad8cecd882
Christina Loiacono <loiaconochristina@gmail.com>
2021-09-22 12:54:09 -0400

    removed span element from Read Time in featured post

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ let firstPost = document.querySelector("header nav ul li");
 // console.log(firstPost);
 firstPost.remove();
 
+let readTime = document.querySelector(".featured article aside p span");
+// console.log(readTime);
+readTime.remove();
+
