25df18dd448d4a6a7073bcb7989c433d938ec10b
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-22 13:51:42 -0400

    Fixed lab

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 document.querySelector("span").remove()
 document.querySelector("li").remove()
 
+const removeH3 = document.querySelectorAll("main .posts article");
+for (let rem of removeH3) {
+    if (rem.textContent.includes("Stop Planning")){
+        rem.remove();
+    }
+};
+
 document.querySelectorAll("main .posts h3").forEach(header => header.remove())
-document.querySelector("main .posts article").remove()
+
