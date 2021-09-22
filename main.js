59209771b6e8110c8909871085dc321f9441f77f
vanessa conti <vanessaconti@pursuit.org>
2021-09-22 13:55:46 -0400

    finished lab

---
+++ main.js
@@ -0,0 +1,12 @@
+const post = document.querySelector("li");
+post.remove();
+
+const read = document.querySelector(".featured span");
+read.remove();
+
+const articles = document.querySelectorAll("article");
+articles[2].remove();
+
+const titles = document.querySelectorAll("h3");
+titles[0].remove();
+titles[1].remove();
