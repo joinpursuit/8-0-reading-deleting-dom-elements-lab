a5709ed45b9433d98f3b936b78a3ff615628d1c1
Jalamang <Jalamangjanka@gmail.com>
2021-09-22 13:20:58 -0400

    Reading & deleting DOM elments

---
+++ main.js
@@ -0,0 +1,14 @@
+const posts = document.querySelector('li');
+posts.remove();
+
+const timeSpans = document.querySelectorAll('span')
+timeSpans.forEach(timeSpan => timeSpan.remove())
+
+const removeLastPost = document.querySelectorAll('main .posts article')
+removeLastPost[removeLastPost.length -1].remove();
+
+const noPosts = document.querySelectorAll('main .posts h3')
+noPosts.forEach(noPost => noPost.remove() );
+
+
+
