0a7a76217cfea26d8a1973e54f255d6a50ba6bdc
james russell <jamesrussell@pursuit.org>
2021-09-22 13:39:54 -0400

    added notes

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ document.querySelector("header nav ul li").remove()
 // removes first span in featured section
 document.querySelector(".featured span").remove()
 
+// removes last article
 let allArticles = document.querySelectorAll("main .posts article")
 allArticles[allArticles.length -1].remove()
 
-
+// removes all post titles
 let elementTitles = document.querySelectorAll("main .posts h3")
 for(let title of elementTitles) {
     title.remove()
