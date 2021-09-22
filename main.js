1e72c5e088d886e99be4e96057114a765ca33a31
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2021-09-22 13:17:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector("li").remove();
+
+document.querySelector("span").remove();
+
+const article = document.querySelectorAll("article")
+article[article.length-1].remove();
+
+const h3 = document.querySelectorAll("h3")
+for (titles of h3) {
+    titles.remove()
+}
