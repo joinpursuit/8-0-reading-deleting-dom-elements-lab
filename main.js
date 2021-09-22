8432e3e6d723a2b827940ce793b15910e6b723c8
Christina Loiacono <loiaconochristina@gmail.com>
2021-09-22 13:11:09 -0400

    removed last post from page - Stop Planning

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ let readTime = document.querySelector(".featured article aside p span");
 // console.log(readTime);
 readTime.remove();
 
+
+let lastPost = document.querySelector("main .posts article")
+// console.log(lastPost);
+lastPost.remove();
