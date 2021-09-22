9974b53e61f2797d90a72f89f8f47f1505259d98
Christina Loiacono <loiaconochristina@gmail.com>
2021-09-22 13:18:50 -0400

    removed all titles from non-featured posts

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ let readTime = document.querySelector(".featured article aside p span");
 readTime.remove();
 
 
-let lastPost = document.querySelector("main .posts article")
+let lastPost = document.querySelector("main .posts article");
 // console.log(lastPost);
 lastPost.remove();
+
+let allTitlesNonFeatured = document.querySelectorAll(".posts article h3");
+// console.log(allTitlesNonFeatured);
+for (let titlesNonFeatured of allTitlesNonFeatured) {
+    // console.log(titlesNonFeatured);
+    titlesNonFeatured.remove();
+}
