ade5bcbb0e424d838000040f426d552dcca44dea
BennyZ3 <bennyz3@hotmail.com>
2021-09-22 12:31:21 -0400

    testing since cypress not running

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector("li").remove();
+document.querySelector("span").remove();
+document.querySelectorAll("article")[3].remove();
+const articlesH3 = document.querySelectorAll("article h3");
+articlesH3.forEach((item) => item.remove());
