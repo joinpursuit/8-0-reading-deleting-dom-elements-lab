3588704015687a42995ab4229c9ee29af356533d
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-22 13:17:18 -0400

    Lab Completed

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector("span").remove()
+document.querySelector("li").remove()
+
+document.querySelectorAll("main .posts h3").forEach(header => header.remove())
+document.querySelector("main .posts article").remove()
