770270462e774a2e28674fb6846707edfa8d205f
justin <justin.d.diaz@gmail.com>
2021-09-22 13:11:46 -0400

    js

---
+++ main.js
@@ -0,0 +1,12 @@
+const post = document.querySelector("li");
+post.remove();
+
+const read = document.querySelector(".featured span");
+read.remove();
+
+const articles = document.querySelectorAll("article");
+articles[2].remove();
+
+const title = document.querySelectorAll("h3");
+title[0].remove();
+title[1].remove();
