e8ae69e92d43d9e806d757f9d29dd0f99dc29f56
kisublee <yigi2021@gmail.com>
2021-09-23 13:11:10 -0400

    Try again.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 
 
-const links = document.querySelectorAll("header nav ul li")
+const links = document.querySelectorAll("header nav ul li");
 const removelink = links[0].remove();
 
-const span = document.querySelector(".featured aside span")
+const span = document.querySelector(".featured aside span");
 
 const removeSpan = span.remove();
 
@@ -12,7 +12,7 @@ const post = document.querySelectorAll(".posts article")
 
 const removeLastPost = post[post.length-1].remove();
   
-const title = document.querySelectorAll(".posts article h3")
+const title = document.querySelectorAll(".posts article h3");
 
-const removeAllTitle = title.forEach((title) => title.remove())
+const removeAllTitle = title.forEach((title) => title.remove());
 
