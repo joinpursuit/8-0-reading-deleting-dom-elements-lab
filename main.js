c5a7852db26e9c9016fa3e86dd77b36e90d4de7f
David Vidals <david.vidals08@gmail.com>
2021-09-24 12:39:45 -0400

    Updated commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let link = document.querySelector('li','ul')
+link.remove();
+
+let span = document.querySelector('span')
+span.remove();
+
+let posts = document.querySelector('.posts article')
+posts.remove();
+
+let header = document.querySelector('main .posts h3');
+header.remove();
+let index = document.querySelector('main .posts h3');
+index.remove();
