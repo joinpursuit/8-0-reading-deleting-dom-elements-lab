ae0d60e115f41851ec1b0f2f300044c05e728d1a
Milton Clark <kingcoder247@gmail.com>
2021-10-02 15:23:17 -0400

    Removed Read Time span

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+///////////Removes ALL Posts Link
 let allPosts = document.querySelector("nav ul li");
 allPosts.remove();
+///////////Removes ALL Posts Link
+
+///////////Removes Read Time Span
+let readTimeSpan = document.querySelector(".featured span");
+readTimeSpan.remove();
+///////////Removes Read Time Span
+
