86c4fbed81d00d67b20d222b49938098566e53e9
Milton Clark <kingcoder247@gmail.com>
2021-10-02 16:41:30 -0400

    Removed Titles from non Featured Posts -  all test passed

--- main.js
+++ main.js
@@ -8,8 +8,14 @@ let readTimeSpan = document.querySelector(".featured span");
 readTimeSpan.remove();
 ///////////Removes Read Time Span
 
-
 ///////////Removes Last Post 
 let stopPlanningPost = document.querySelectorAll(".posts article");
 stopPlanningPost[2].remove();
 ///////////Removes Last Post 
+
+///////////Removes Titles from non featured posts 
+let h3Titles = document.querySelectorAll(".posts h3");
+for(let el of h3Titles){  //Use for loop to loop through each title in the node list to remove.
+    el.remove();
+}
+///////////Removes Titles from non featured posts 
