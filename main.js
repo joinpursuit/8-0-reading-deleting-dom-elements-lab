bb7bf867dafa0f3b9f61227dfbf4f3f45e09b37a
named.josie <named.josie@gmail.com>
2022-03-25 15:11:05 -0400

    update file

---
+++ main.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('li');
+header.remove();
+const span = document.querySelector('span');
+span.remove()
+const article = document.querySelector('.posts article')
+article.remove();
