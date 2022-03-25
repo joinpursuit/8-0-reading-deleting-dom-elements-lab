5e1739ce2d3ecdc054bc3cfae1ed6e37312d14f5
TaryneLeach <taryneleach@gmail.com>
2022-03-25 14:43:08 -0400

    Reading-Deleting-Dom

---
+++ main.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const removeAllPosts = document.querySelector('header nav ul li');
+removeAllPosts.remove();
+
+const readTimeSpan = document.querySelector('.featured span');
+readTimeSpan.remove();
+
+const lastPost = document.querySelector('.posts article');
+lastPost.remove();
+
+const allTitles = document.querySelectorAll('.posts article h3');
+allTitles.forEach((title) => {
+	title.remove();
+});
+``
