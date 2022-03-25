d1c2b9b7ccaf9dffcdf032bfd65631bde29d5895
Andres Varon <anvaron@gmail.com>
2022-03-25 18:01:48 -0400

    test-03: template tested

--- scripts.js
+++ scripts.js
@@ -2,3 +2,5 @@
 document.querySelector("header ul li").remove();
 // => Test-02: Removing span element on festured article
 document.querySelector(".featured aside p span").remove();
+// => Test-03: Removing last article on category posts
+document.querySelector(".posts article:last-child").remove();
