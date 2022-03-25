aff5563b93452ab83c59b182bf441cf224d58c79
Andres Varon <anvaron@gmail.com>
2022-03-25 18:01:03 -0400

    test-02: template tested

--- scripts.js
+++ scripts.js
@@ -1,2 +1,4 @@
-// => Removing the first list item of the menu
+// => Test-01: Removing the first list item of the menu
 document.querySelector("header ul li").remove();
+// => Test-02: Removing span element on festured article
+document.querySelector(".featured aside p span").remove();
