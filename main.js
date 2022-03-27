946875a5de2760057d8b80308aba7b830d3b7ab5
Jamal Ruiz <jamalruiz@jamals-air-2.mynetworksettings.com>
2022-03-27 16:01:38 -0400

    Final Commit

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const allPost = document.querySelector('header li').remove();
 // allPost.remove();
 const span = document.querySelector('.featured span').remove()
-const lastPost = document.querySelector("main .posts article").remove()
+const lastPost = document.querySelectorAll("main .posts article");
+lastPost[lastPost.length-1].remove()
+// const lastPost = document.querySelector("main .posts article").remove() ------ WORKS BUT IS NOT CORRECT WAY
 const removeAll = document.querySelectorAll("main .posts h3");
 removeAll.forEach((remove) => {
     remove.remove()
