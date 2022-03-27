b40853a21937f7a70931f886391044dc2c860f2c
lchen6283 <lchen6283@gmail.com>
2022-03-27 12:07:53 -0400

    update

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ header.remove();
 const span = document.querySelector(".featured span");
 span.remove();
 
-const post = document.querySelector("main .posts article");
-post.remove();
+const post = document.querySelectorAll("main .posts article");
+post[post.length - 1].remove();
 
 const posts2 = document.querySelectorAll("main .posts h3");
 posts2.forEach((title) => {
