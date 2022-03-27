c29c2e3a6898bd20818dbf35aa7845b7fb736ed3
Matt M <Afrodesiacc@gmail.com>
2022-03-27 18:59:40 -0400

    Completed Work.

---
+++ main.js
@@ -0,0 +1,20 @@
+let oldPost = document.querySelector("li");
+
+    oldPost.remove()
+
+let removedSpan = document.querySelector("span")
+
+    removedSpan.remove()
+
+let removedPost = document.querySelector(".posts article")
+
+    removedPost.remove()
+
+let removedTitles = document.querySelectorAll(".posts h3")
+    removedTitles.forEach((title) => {
+        title.remove()
+    })
+
+
+
+
