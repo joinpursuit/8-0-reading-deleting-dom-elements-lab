392e4256266bc3d0ef082b3aab9b9cf84457cd0b
luke kinoshi <lukekinoshi@lukes-MacBook-Air.local>
2022-03-27 16:31:05 -0400

    lukeremoval

---
+++ main.js
@@ -0,0 +1,15 @@
+const removal = document.querySelector("header nav ul li")
+removal.remove();
+
+const removal2 = document.querySelector("main section aside p span")
+removal2.remove();
+
+const removal3 = document.querySelector("main .posts article")
+removal3.remove();
+
+const removal4 = document.querySelector("main .posts h3")
+removal4.remove();
+
+const removal5 = document.querySelector("main .posts h3")
+removal5.remove();
+
