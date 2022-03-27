f4d27e9b3c993b3a7a7c787984f9e8e964127d53
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-03-27 18:24:40 -0400

    DOM

---
+++ main.js
@@ -0,0 +1,10 @@
+const header = document.querySelector("li");
+header.remove();
+const readm = document.querySelector('span');
+readm.remove();
+const readmy = document.querySelector('main .posts article');
+readmy.remove();
+const readmyp = document.querySelector('main .posts h3');
+readmyp.remove();
+const readmypg = document.querySelector('main .posts h3');
+readmypg.remove();
