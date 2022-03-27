d2e44500dc41ef162faaa1e62c6e9db07f7e2910
Stephanie Frias <steph.frias01@me.com>
2022-03-27 10:06:13 -0400

    completed

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+
 const allPosts = document.querySelector("li");
 const featuredSpan = document.querySelector("span");
 allPosts.remove();
 featuredSpan.remove();
+
+const lastPost = document.getElementsByClassName("posts");
+for (let post of lastPost) {
+  post.lastElementChild.remove();
+}
+
+const nonFeatured = document.querySelectorAll(".posts article h3");
+nonFeatured.forEach((heading) => {
+  heading.remove();
+});
+
