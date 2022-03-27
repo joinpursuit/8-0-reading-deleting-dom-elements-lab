c1de400bdddbbf8bbc6974d486e419ed0ecd0157
John Prado <jcprado20@icloud.com>
2022-03-27 16:30:49 -0400

    lab done

---
+++ main.js
@@ -0,0 +1,14 @@
+const gone = document.querySelector("header nav ul li");
+gone.remove();
+
+const gone2 = document.querySelector("main section aside p span");
+gone2.remove();
+
+const gone3 = document.querySelector("main .posts article");
+gone3.remove();
+
+const gone4 = document.querySelector("main .posts h3");
+gone4.remove();
+
+const gone5 = document.querySelector("main .posts h3");
+gone5.remove();
