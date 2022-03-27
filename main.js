f71171b47c2beaf3dcfee0754facf96eb9db0fe5
Tom LaTulipe <tlatulipe@gmail.com>
2022-03-27 15:19:38 -0400

    everything looks good - this stuff is hard

---
+++ main.js
@@ -0,0 +1,19 @@
+//const upAnchor = document.querySelector('header li');
+//upAnchor.remove("All posts");
+
+document.querySelector('li').remove();
+
+//const spanner = document.querySelector('span');
+//spanner.remove("Read Time");
+
+document.querySelector('span').remove();
+
+//const eraseLastPost = document.querySelector('main .posts article');
+//eraseLastPost.remove("Stop Planning");
+const subPosts = document.querySelectorAll('.posts article');
+subPosts[subPosts.length-1].remove();
+
+let titleWave = document.querySelectorAll('main .posts h3');
+titleWave.forEach((title) => {
+    title.remove(title);
+});
