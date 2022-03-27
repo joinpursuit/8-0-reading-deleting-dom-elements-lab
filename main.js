6402c6f0946222b15d6793616bbbefb87d6406de
Jimmy Ayivor <jimmyayivor@gmail.com>
2022-03-27 12:54:54 -0400

    8-0-reading-deleting-dom-elements-lab

---
+++ main.js
@@ -0,0 +1,10 @@
+const allPost = document.querySelector('li');
+allPost.remove();
+const readTime = document.querySelector('span');
+readTime.remove();
+const articles = document.querySelectorAll('article');
+stopPlanning = articles[articles.length-1];
+stopPlanning.remove();
+
+document.querySelectorAll('.posts h3'.remove();
+
