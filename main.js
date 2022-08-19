8b23a01658f5f4844e540e6cb7a8db9c2f6f277a
MarsiaRochelin <marsrochelin@pursuit.org>
2022-08-19 15:16:21 -0400

    update 3

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ allPost.remove()
 const readTime = document.querySelector('span')
 readTime.remove()
 
-// const stop1 = document.querySelector('main .posts article')
-// stop1.remove()
+const stop1 = document.querySelector('main .posts article')
+stop1.remove()
 
 // const notFeatured = document.querySelectorAll('main .posts h3')
 // notFeatured.forEach((featured) => featured.remove())
