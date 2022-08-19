e300aa762bf387b24ab450dbdc172c760a20c4f6
MarsiaRochelin <marsrochelin@pursuit.org>
2022-08-19 15:15:52 -0400

    update 2

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const allPost = document.querySelector('li')
 allPost.remove()
 
-// const readTime = document.querySelector('span')
-// readTime.remove()
+const readTime = document.querySelector('span')
+readTime.remove()
 
 // const stop1 = document.querySelector('main .posts article')
 // stop1.remove()
