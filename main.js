11a7f94804bc5f9bacd9ad2e3e835b1374c10aaa
Salinamalek <salinamalek@pursuit.org>
2022-08-19 13:16:52 -0400

    3/4 tests passed

---
+++ main.js
@@ -0,0 +1,13 @@
+const removeLink = document.querySelector('header nav ul li')
+removeLink.remove()
+
+
+const removeSpan = document.querySelector('.featured span')
+removeSpan.remove()
+
+
+const removeArticle = document.querySelector('main .posts article')
+removeArticle.remove()
+
+// const removeAllTitle = document.querySelectorAll('main .posts h3')
+// removeAllTitle.remove()
