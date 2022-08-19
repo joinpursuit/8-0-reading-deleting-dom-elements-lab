15968c7b0343f6778f1b4f7e7a761cbbc7989288
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-08-19 13:51:30 -0400

    tests passing

---
+++ main.js
@@ -0,0 +1,11 @@
+const allPosts = document.querySelector(`header li`);
+allPosts.remove();
+
+const readTime = document.querySelector(`.featured span`);
+readTime.remove();
+
+const posts = document.querySelectorAll(`.posts article`);
+posts[posts.length - 1].remove();
+
+const postTitles = document.querySelectorAll(`.posts h3`)
+postTitles.forEach(title => title.remove())
