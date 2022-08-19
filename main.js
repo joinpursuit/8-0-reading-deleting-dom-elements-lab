7ada22db125579fb3c62304d99597260ead19d09
PESolut <jahaadpetty@pursuit.org>
2022-08-19 14:08:16 -0400

    two of four tests passed

---
+++ main.js
@@ -0,0 +1,11 @@
+const allPosts = document.querySelector('li')
+allPosts.remove()
+console.log(allPosts)
+
+const span = document.querySelector('span')
+console.log(span)
+span.remove()
+
+const allTitles = document.querySelectorAll('main .posts h3')
+console.log(allTitles)
+// allTitles.remove()
