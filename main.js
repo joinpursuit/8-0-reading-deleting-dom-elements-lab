64b23011064d577c8f102fe25869e9edd8c63fea
TonyH98 <tonyhoang@pursuit.org>
2022-08-19 13:20:36 -0400

    All test passed

---
+++ main.js
@@ -0,0 +1,14 @@
+const header = document.querySelector(`header nav ul li`)
+console.log(header.remove())
+
+const span = document.querySelector(`.featured span `)
+console.log(span.remove())
+
+const article = document.querySelector(`main .posts article`)
+console.log(article.remove())
+
+ const title = document.querySelector(`main .posts h3`)
+ console.log(title.remove())
+
+ const titles = document.querySelector(`main .posts h3`)
+ console.log(titles.remove())
