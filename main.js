b2cb73616721b255428baa13253fb06785eecc29
Richie-Pursuit <richiecarrasco@pursuit.org>
2022-08-22 02:13:18 -0400

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+const allPosts = document.querySelector('li');
+allPosts.remove();
+
+const readTime = document.querySelector('.featured span');
+readTime.remove();
+
+const stopPlanning = document.querySelector(' .posts article');
+stopPlanning.remove();
+
+const nonFeaturedPost = document.querySelectorAll(' main .posts h3');
+nonFeaturedPost.forEach((x) => x.remove());
