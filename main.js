9666734d82a3756a37e23018e6ce0a826eefdffe
shaquala <shaqualafredericks@pursuit.org>
2022-08-21 23:26:04 -0400

    still receiving errors

---
+++ main.js
@@ -0,0 +1,36 @@
+const allPosts= document.querySelector("li")
+//console.log(oldPosts)
+
+allPosts.remove();
+
+
+const readTime = document.querySelector("span");
+//let removedSpan= document.querySelector("span")
+
+readTime.remove();
+
+const articles = document.querySelectorAll("article")
+let test;
+for(let article of articles){
+    test = article;
+}
+test.remove();
+//let removedPost = document.querySelector(".posts article")
+
+//removedPost.remove()
+
+const titles = document.querySelectorAll(" h3")
+for(let title of titles){
+    title.remove()
+  
+    
+}
+
+//removedTitles.forEach((title)=>{
+  //  title.remove()
+
+
+
+
+
+
