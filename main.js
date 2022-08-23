09937c047f1493340860d59f6341027253149c81
SharukhGV <sharukhvelupillai@pursuit.org>
2022-08-23 18:48:18 -0400

    finished

---
+++ main.js
@@ -0,0 +1,39 @@
+const apple = document.querySelector("header nav ul li");
+apple.remove();
+// const pear = document.querySelector('main>section');
+// pear.remove()
+
+
+// let highestParentSoon = document.querySelectorAll("h2");
+// let lowestChildSoon = document.querySelectorAll("h2");
+
+// let adultvar = function(){
+//  while(highestParentSoon!==null){
+// let adulttt =  highestParentSoon.append(".parentElement")};
+// return adulttt;
+//  }
+
+//  let childvar = function(){
+//     while(lowestChildSoon!==null){
+//    let firssschild = highestParentSoon.append(".firstChild")};
+//    return firssschild;
+//     }
+const doggo = document.querySelector(".featured span");
+doggo.remove();
+
+
+//  if(!adultvar.class === "featured"){
+    
+//  }
+
+
+const helloq = document.querySelector('main .posts article');
+helloq.remove();
+
+
+
+const googale = document.querySelector('main .posts h3');
+googale.remove();
+
+const googale2 = document.querySelector('main .posts h3');
+googale2.remove();
