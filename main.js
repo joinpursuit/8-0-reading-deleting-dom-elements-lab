0e11ba6c2eac3ae4f4d1f07d28c9bd4370da87d7
TrianePeart <TrianePeart@gmail.com>
2022-08-25 10:34:31 -0400

    Lab completed

---
+++ main.js
@@ -0,0 +1,16 @@
+const altPost = document.querySelector("li")
+
+altPost.remove()
+
+const span = document.querySelector("span")
+
+span.remove()
+
+const lastPost = document.querySelector("main .posts article")
+
+lastPost.remove()
+
+
+ const title = document.querySelectorAll("h3")
+
+title.forEach((titles) => titles.remove()) 
