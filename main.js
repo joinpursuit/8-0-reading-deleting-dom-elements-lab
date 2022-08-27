59df7394506b23fe8c82ace49122f16cd9e8699b
bigdaddyjean <jeannappoleon@pursuit.org>
2022-08-27 11:30:57 -0400

    finished

---
+++ main.js
@@ -0,0 +1,16 @@
+const span = document.querySelector("span");
+span.remove();
+
+const allPosts = document.querySelector("header nav ul li");
+allPosts.remove();
+
+const last = document.querySelector("main .posts article");
+last.remove();
+
+const noTitles = document.querySelector("main .posts h3");
+noTitles.remove();
+
+
+
+
+
