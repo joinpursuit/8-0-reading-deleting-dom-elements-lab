2c437ae4e4b32f70efe4fabde9a3a65195c0073e
Wwright91 <wisdomwright-martin@pursuit.org>
2022-08-29 06:22:55 -0400

    fixed-changes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.querySelector("li").remove();
 document.querySelector("span").remove();
-document.querySelector("main .posts article").remove();
+lastPost = document.querySelectorAll("main .posts article");
+lastPost[lastPost.length - 1].remove()
 const titles = document.querySelectorAll("main .posts h3");
 titles.forEach(t => t.remove())
