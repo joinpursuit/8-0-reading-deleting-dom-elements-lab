85e7b85621ba3d1ad598bc105280dc1562a8e537
PESolut <jahaadpetty@pursuit.org>
2022-09-27 04:37:53 -0400

    All tests passed

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const span = document.querySelector('span')
 console.log(span)
 span.remove()
 
-const allTitles = document.querySelectorAll('main .posts h3')
-console.log(allTitles)
-// allTitles.remove()
+const allTitles = document.querySelectorAll('h3')
+allTitles.forEach(title => {
+    title.remove();
+})
+
+const allArticles = document.querySelectorAll('article')
+console.log(allArticles)
+allArticles[allArticles.length-1].remove()
