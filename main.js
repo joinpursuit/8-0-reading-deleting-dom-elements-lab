56281535b5e86b979486d7c7f85a60c02ea767c3
BertCasale <bthebest55@gmail.com>
2022-12-12 19:40:56 -0500

    next 2 steps complete

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 //remove the first list item
-document.querySelector("li").remove()
+document.querySelector("li").remove();
 
+//remove the first span element
+document.querySelector("span").remove();
+
+//get all the posts
+const posts = document.querySelectorAll("article");
+//remove the last post
+posts[posts.length - 1].remove();
