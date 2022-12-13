ec5696d4d48109df7247438d54df7616155218aa
Amber King <amber.king9292@gmail.com>
2022-12-13 14:36:05 -0500

    R-D-Dom Elements Lab-3 out of 4 Passed

---
+++ main.js
@@ -0,0 +1,15 @@
+const noPosts = document.querySelector('li')
+// ^---variablt sets to select into the the first element w/li label
+
+noPosts.remove()
+// ^--.remove() the variable to remove desire section
+
+const noSpan = document.querySelector('.featured span')
+noSpan.remove()
+
+
+const noLastPost = document.querySelector('main .posts article')
+noLastPost.remove()
+
+const noPostAtAll = document.querySelector('main .posts h3')
+noPostAtAll.remove()
