16ed8fe26c1acb13c604e50beb43191095139703
RadiantRay22 <rayrussell@pursuit.org>
2022-12-12 21:38:39 -0500

    RayDone

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector("a")
+const allPost= document.querySelector("li");li
+allPost.remove() //have to remove using variable 
+const readTime= document.querySelector("span");
+readTime.remove //have to remove using the variable
+const lastPost = document.querySelectorAll('article');
+lastPost[2].remove /*look into the html article, and count
+the index position*/
+const titles= document.querySelectorAll('h3')
+// this is a NODE LIST it requires iteration
+titles.forEach(title)
+title.remove 
