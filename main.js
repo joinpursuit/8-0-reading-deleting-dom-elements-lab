ca16b4851c44bf4d91facee4f0144aa1be1d64b1
k1ll3rwh4le15 <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>
2022-12-13 12:35:51 -0500

    commits index & main

---
+++ main.js
@@ -0,0 +1,13 @@
+const posts = document.querySelector('li');
+posts.remove();
+
+const span = document.querySelector("span");
+span.remove();
+
+const lastPost = document.querySelector("main .posts article");
+lastPost.remove();
+
+const noTitle = document.querySelectorAll("h3");
+noTitle.forEach((title) =>{
+    title.remove();
+});
