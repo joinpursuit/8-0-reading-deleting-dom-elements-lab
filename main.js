f7197418bce10b38680b56f7efbfefdc8317cb6e
aardvarkpepper <aardvarkpepper@gmail.com>
2022-12-13 12:44:43 -0500

    Remove comments to attempt to pass CircleCI test.

--- main.js
+++ main.js
@@ -2,20 +2,6 @@ let var1 = document.querySelector("header nav ul li");
 var1.remove();
 let var2 = document.querySelector(".featured article aside p span");
 var2.remove();
-// document.querySelector("main .posts article").remove(); => notify Raydelys of solution
-// document.querySelector([document.querySelector("article").length])
-// document.querySelectorAll("article").
-
-//let var3 = document.querySelectorAll("article");
-// const lastPost = document.querySelectorAll("article")
-// lastPost[2].remove();
-
-// stopPlanning[stopPlanning.length-1.remove();]
-// lastChild
-
-
-// const jeez = document.querySelector("section.posts")
-// jeez.remove();
 
 let var3 = document.querySelectorAll("article");
 var3[var3.length-1].remove();
