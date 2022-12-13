50775b722ab4cebb2b2db72d6e0bb2f266ad6977
Colin Jaffe <balloonasaurus@gmail.com>
2022-12-13 09:17:12 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector('header li').remove()
+document.querySelector('.featured span').remove()
+document.querySelector('.posts article:last-child').remove()
+document.querySelectorAll('article h3').forEach((h3) => h3.remove())
