5609b5058bcfc9fdf2cc534f9e88b136d69769bf
SangUn Park <Sangunpark1@gmail.com>
2022-12-13 11:04:40 -0500

    Js File Lab

---
+++ main.js
@@ -0,0 +1,13 @@
+const allPost = document.querySelector("li"); allPost.remove()
+
+ const spanReadTime = document.querySelector("span");
+ spanReadTime.remove()
+
+
+ const lastPost = document.querySelectorAll("article");
+ lastPost[lastPost.length-1].remove()
+
+ const allHeading = document.querySelectorAll("h3")
+ allHeading.forEach((allHeading) => {
+     allHeading.remove()
+ })
