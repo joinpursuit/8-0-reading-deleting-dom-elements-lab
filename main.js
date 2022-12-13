fd1802cc8d62de17b87b281ff3a10317af12cf98
aardvarkpepper <aardvarkpepper@gmail.com>
2022-12-13 12:33:34 -0500

    Alter code to attempt to pass CircleCI test.  Introduce holding variables var1 and var2 then operate to remove.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-document.querySelector("li").remove();
-document.querySelector("span").remove();
+let var1 = document.querySelector("li");
+var1.remove();
+let var2 = document.querySelector("span");
+var2.remove();
 // document.querySelector("main .posts article").remove(); => notify Raydelys of solution
 // document.querySelector([document.querySelector("article").length])
 // document.querySelectorAll("article").
 
-//let holderVar = document.querySelectorAll("article");
+//let var3 = document.querySelectorAll("article");
 // const lastPost = document.querySelectorAll("article")
 // lastPost[2].remove();
 
@@ -15,8 +17,8 @@ document.querySelector("span").remove();
 // const jeez = document.querySelector("section.posts")
 // jeez.remove();
 
-let holderVar = document.querySelectorAll("article");
-holderVar[holderVar.length-1].remove();
+let var3 = document.querySelectorAll("article");
+var3[var3.length-1].remove();
 
 const titles = document.querySelectorAll("h3");
 titles.forEach((title) => {
