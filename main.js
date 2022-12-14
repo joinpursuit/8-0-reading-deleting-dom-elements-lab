0d054700f02fc4b8bc1afe5791c35b3b55e4a2e1
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-13 20:42:48 -0500

    Test case 4 finished

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 
-const removeHeaderPost = document.querySelector("li") // or one - line way === document.querySelector("li").remove()
-removeHeaderPost.remove()
+ const removeHeaderPost = document.querySelector("li") // or one - line way === document.querySelector("li").remove()
+ removeHeaderPost.remove()
 
-const removeSpan = document.querySelector("span")
-removeSpan.remove()
+ const removeSpan = document.querySelector("span")
+ removeSpan.remove()
 
-const removeLastPost = document.querySelector(".posts article:last-child")
-removeLastPost.remove()
+ const removeLastPost = document.querySelector(".posts article:last-child")
+ removeLastPost.remove()
 
-const removeTitles = document.querySelectorAll(".posts h3")
-removeTitles.forEach(item => item.remove())
+ const removeTitles = document.querySelectorAll(".posts h3")
+ removeTitles.forEach(item => item.remove())
 
-console.log("hello")
 
-window.alert("hello, what's ur name");
 
-console.log("hello again")
+ // window.alert("hello, what's ur name"); -> alert a message as soon as the screen loads
+
+ // console.log("hello again")
