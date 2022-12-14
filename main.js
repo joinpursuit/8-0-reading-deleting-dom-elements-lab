8e4a3713a76ba1a036eed20bacc7f37952391d88
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-13 20:31:39 -0500

    Test case 3 finished

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 
-const post = document.querySelector("li")
-post.remove();
-
-//document.querySelector("li").remove()
+const removeHeaderPost = document.querySelector("li") // or one - line way === document.querySelector("li").remove()
+removeHeaderPost.remove()
 
 
 const removeSpan = document.querySelector("span")
 removeSpan.remove()
+
+const removeLastPost = document.querySelector(".posts article:last-child")
+removeLastPost.remove()
