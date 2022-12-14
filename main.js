b5a9f4105edd4b1b8f843c4638f469c8d95334ba
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-13 20:41:30 -0500

    Test case 3 finished

--- main.js
+++ main.js
@@ -10,3 +10,9 @@ removeLastPost.remove()
 
 const removeTitles = document.querySelectorAll(".posts h3")
 removeTitles.forEach(item => item.remove())
+
+console.log("hello")
+
+window.alert("hello, what's ur name");
+
+console.log("hello again")
