5f037db2f8d447a8b0d50792548caaf6d7f7c2f8
Diegocarrero <diegocar2413@gmail.com>
2023-01-13 16:35:19 -0500

    Fixed 3rd objecti ve

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 document.querySelector("li").remove();
+
 document.querySelector("span").remove();
+
 const lastPost = document.querySelectorAll('article');
-lastPost[2].remove();
+lastPost[3].remove();
+
 const titles = document.querySelectorAll('h3');
 titles.forEach((title) => {
     title.remove();
